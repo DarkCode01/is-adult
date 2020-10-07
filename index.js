58c542eaa139e36807a745e12d84db3b2d66e710
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-10-07 13:54:08 -0400

    feat: option to pass a date to compare

--- index.js
+++ index.js
@@ -7,26 +7,45 @@
 
 'use strict';
 
-module.exports = function isAdult(day, month, year, options={ olderNumber: 18 }) {
-  day = Math.abs(day);
-  month = Math.abs(month);
-  year = Math.abs(year);
+const getCurrentDate = require('./utils/currentDate');
 
-  if (!Number.isInteger(day) || !Number.isInteger(month) || !Number.isInteger(year)) {
+/**
+ * Function to validate is a Date is adult.
+ *
+ * @param {string|number} day - day of month.
+ * @param {string|number} month - month.
+ * @param {string|number} year - full year [ex: 2020]
+ * @param {object} [options={ olderNumber: 18 }] - full year [ex: 2020]
+ * @return {boolean}
+ *
+ * @example
+ *
+ *     isAdult(1, 1, 1998)  //=> true
+ */
+function isAdult(day, month, year, options={ olderNumber: 18, dateToCompare: getCurrentDate() }) {
+  // parser options
+  const olderNumberToCompare = options.olderNumber || 18;
+  const [currentDay, currentMonth, currentYear] = options.dateToCompare || getCurrentDate();
+
+  // parsing dates
+  const validateDay = Math.abs(day);
+  const validateMonth = Math.abs(month);
+  const validateYear = Math.abs(year);
+
+  // validating date
+  if (!Number.isInteger(validateDay) || !Number.isInteger(validateMonth) || !Number.isInteger(validateYear)) {
     throw new Error('expected an integer');
   }
-
-  if (!Number.isSafeInteger(day) || !Number.isSafeInteger(month) || !Number.isSafeInteger(year)) {
+  if (!Number.isSafeInteger(validateDay) || !Number.isSafeInteger(validateMonth) || !Number.isSafeInteger(validateYear)) {
     throw new Error('value exceeds maximum safe integer');
   }
+  
+  let age = currentYear - (validateYear || currentYear);
 
-  const date = new Date();
-  const currentDay = date.getDate();
-  const currentMonth = date.getMonth();
-  const currentYear = date.getFullYear();
-  let age = currentYear - (year || currentYear);
+  // validate if passed his birthday
+  if (validateMonth >= currentMonth && validateDay >= currentDay) age -= 1;
 
-  if (month >= currentMonth && day >= currentDay) age -= 1;
-
-  return age >= options.olderNumber;
+  return age >= olderNumberToCompare || 28;
 }
+
+module.exports = isAdult;
