b00808684e764f55e692f28b066ee93c695bf966
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-10-08 11:10:58 -0400

    added tests cases

--- utils/currentDate.js
+++ utils/currentDate.js
@@ -1,16 +1,17 @@
 'use strict';
 
 /**
- * getCurrentDate: get the current formatter a array of numbers
+ * parserDate: parser a Date passed as argument
+ * if not pass a Date return a currentDate.
  *
+ * @param {Date} [date=Date]
  * @return {number[]} [day, month, year]
  *
  * @example
  *
+ *     getCurrentDate(new Date('1995-12-17T03:24:00'))  //=> [17, 12, 1995]
  *     getCurrentDate()  //=> [7, 10, 2020]
  */
-module.exports = function getCurrentDate() {
-  const date = new Date();
-
+module.exports = function parserDate(date=new Date()) {
   return [ date.getDate(), date.getMonth(), date.getFullYear() ];
 }
